03ab3aea205ddaafd61abcfb28832203bd78a2b7
vbrosso <vbrosso@gmail.com>
2016-04-11 11:20:42 -0300

    ajuste no js para fonte padrao de acessibilidade

--- src/nfe/portal/static/js/main.js
+++ src/nfe/portal/static/js/main.js
@@ -118,7 +118,7 @@ $(document).ready(function(){
             $('body').css("font-size",maisUm+"px");
         }
         function resetFont(){
-            $('body').css("font-size","19px");
+            $('body').css("font-size","16px");
 
             $.removeCookie('contraste2', { path: '/' });
             $.removeCookie('contraste1', { path: '/' });
